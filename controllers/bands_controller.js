0d723a34d16a55cdefce46c40e145f35142d6666
davechurilla <churilla@gmail.com>
2022-11-12 20:48:34 -0800

    add bands controller

--- controllers/bands_controller.js
+++ controllers/bands_controller.js
@@ -2,12 +2,30 @@
 const bands = require("express").Router();
 const db = require("../models");
 const { Band } = db;
+const { Op } = require("sequelize");
 
 // FIND ALL BANDS
 bands.get("/", async (req, res) => {
   try {
-    const foundBands = await Band.findAll();
+    const foundBands = await Band.findAll({
+      order: [["available_start_time", "ASC"]],
+      where: {
+        name: { [Op.like]: `%${req.query.name ? req.query.name : ""}%` },
+      },
+    });
     res.status(200).json(foundBands);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// FIND A SPECIFIC BAND
+bands.get("/:id", async (req, res) => {
+  try {
+    const foundBand = await Band.findOne({
+      where: { band_id: req.params.id },
+    });
+    res.status(200).json(foundBand);
   } catch (error) {
     res.status(500).json(error);
   }
